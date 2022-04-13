2254e768cc433933438fe418d189d0ba16b5d201
unknown <stubbornsachin@gmail.com>
2022-04-13 12:27:11 +0530

    pdf viewer width increased

--- src/components/operatorView/index.js
+++ src/components/operatorView/index.js
@@ -36,6 +36,7 @@ function OperatorViewPage (props) {
 
 
   const openDocument = (document) => {
+    console.log("Doc",document);
     setSelectedDocument(document);
     openPopup()
     // setNumPages(null);
@@ -99,8 +100,8 @@ function OperatorViewPage (props) {
       </div>
       {/*popup to show document*/}
       <div id="operatorDocumentPopup" className="nj-overly add-rebound-animation" >
-          <div className="doc-popup my-popup" style={{maxWidth:700, marginTop:'5vh'}}>
-            <div className="doc-popup-form" style={{maxWidth:700, overflow:'auto'}}>
+          <div className="doc-popup my-popup" style={{maxWidth:'100%', marginTop:'5vh'}}>
+            <div className="doc-popup-form" style={{maxWidth:'100%', overflow:'auto'}}>
                 <div className="doc-popup-form__inner">
                   <div className="doc-popup-title">
                     <span onClick={hidePopUp} className="doc-popup__close">
